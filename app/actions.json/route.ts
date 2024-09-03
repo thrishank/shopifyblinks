a7f3d49e5340b9011be3e9626721eeebadf14be2
thrishank <thrishankkalluru@gmail.com>
2024-09-03 23:00:29 +0530

    set up the project structure

---
+++ app/actions.json/route.ts
@@ -0,0 +1,24 @@
+import { ActionsJson, createActionHeaders } from "@solana/actions";
+
+export const GET = async () => {
+  const payload: ActionsJson = {
+    rules: [
+      // map all root level routes to an action
+      {
+        pathPattern: "/*",
+        apiPath: "/api/actions/*",
+      },
+      // idempotent rule as the fallback
+      {
+        pathPattern: "/api/actions/**",
+        apiPath: "/api/actions/**",
+      },
+    ],
+  };
+
+  return Response.json(payload, {
+    headers: createActionHeaders(),
+  });
+};
+
+export const OPTIONS = GET;
---
+++ shopify.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  const token = searchParams.get("token");
+  const shop_url = searchParams.get("shop_url");
+
+  console.log(token, shop_url);
+  if (typeof token !== "string" || typeof shop_url !== "string") {
+    return NextResponse.json(
+      { error: "Invalid token or website_url" },
+      { status: 400 }
+    );
+  }
+
+  let url = `https://${shop_url}/admin/api/2024-07/products.json?limit=5`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "X-Shopify-Access-Token": token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch products" },
+      { status: 500 }
+    );
+  }
+}
