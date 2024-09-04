6a4560f7732bf5777713c9863518e2570bc99bf8
thrishank <thrishankkalluru@gmail.com>
2024-09-04 12:49:01 +0530

    add nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import NavBar from "@/components/UI/Home/Nav";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={`min-h-screen`}>
+      <div className="bg-gradient-to-b from-purple-50 to-blue-50 dark:from-gray-900 dark:to-purple-900 transition-colors duration-300">
+        <NavBar />
+      </div>
+    </div>
+  );
 }
---
+++ components/UI/Home/Nav.tsx
@@ -0,0 +1,58 @@
+"use client";
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/common/button";
+
+export default function NavBar() {
+  return (
+    <nav className="container mx-auto px-4 py-6 flex justify-between items-center">
+      <div className="text-2xl font-bold text-purple-700 dark:text-purple-300">
+        ShopifyBlinks
+      </div>
+      <div className="flex items-center gap-4">
+        <Button
+          variant="outline"
+          className="bg-white dark:bg-gray-800 dark:text-white"
+        >
+          Login
+        </Button>
+        <Button
+          variant="outline"
+          className="bg-white dark:bg-gray-800 dark:text-white"
+        >
+          Signup
+        </Button>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
+
+function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  const theme = useTheme();
+
+  const handleToggle = () => {
+    if (theme.theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      className="text-purple-700 dark:text-purple-300"
+      onClick={handleToggle}
+    >
+      {theme.theme === "dark" ? (
+        <Sun className="h-5 w-5" />
+      ) : (
+        <Moon className="h-5 w-5" />
+      )}
+    </Button>
+  );
+}
